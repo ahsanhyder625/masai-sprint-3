6bb0e6ff7ebc430a0c9814408af8f5f4f2c7e2cd
Ahsan hyder <39864885+ahsanhyder625@users.noreply.github.com>
2020-06-06 23:32:43 +0530

    4th commit

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-//curl --location --request GET 'https://api.covid19api.com/summary'
-
-
-function func()
-{
+var t=document.querySelector('.table1')
     console.log("before api call");
     const url = 'https://api.covid19api.com/summary';
     fetch(url) // Call the fetch function passing the url of the API as a parameter
@@ -10,12 +6,23 @@ function func()
     .then(function(data) {
         // Your code for handling the data you get from the API
         // console.log(data.Countries)
-        console.log(data.Countries[0])
-})
-    .catch(function() {
-        // This is where you run code if the server returns any errors
-    });
-}
+        for(var i=0;i<data.Countries.length;i++){
+            var tr=document.createElement('tr')
+            var td1=document.createElement('td')
+            var td2=document.createElement('td')
+            var td3=document.createElement('td')
+            var td4=document.createElement('td')
+            td1.textContent=data.Countries[i].Country
+            td2.textContent=data.Countries[i].TotalConfirmed-data.Countries[i].TotalDeaths-data.Countries[i].TotalRecovered
+            td3.textContent=data.Countries[i].TotalRecovered
+            td4.textContent=data.Countries[i].TotalDeaths
+            tr.appendChild(td1)
+            tr.appendChild(td2)
+            tr.appendChild(td3)
+            tr.appendChild(td4)
+            t.appendChild(tr)
+        }
+    })()
 
 
 document.getElementById('update').addEventListener('click',func)
